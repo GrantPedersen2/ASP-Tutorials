e3022a536bde660be83ef3261ec459bb48b9c5ae
GrantPedersen2 <grantpedersen2@live.com>
2020-01-25 16:49:15 -0700

    first commit

---
+++ HelloWorld/HelloWorldMVC/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using HelloWorldMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace HelloWorldMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            AppMessage obj = new AppMessage()
+            {
+                Message = "Hello World"
+            };
+            return View(obj);
+        }
+
+        //Overload called when HTML form is POSTed to server...
+        [HttpPost]
+        public IActionResult Index(AppMessage obj)
+        {
+            ViewBag.Message = "Message changed.";
+            return View(obj);
+        }
+    }
+}
---
+++ HelloWorld/HelloWorldWebApi/Models/AppMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldMVC.Models
+{
+    public class AppMessage
+    {
+        //Represent message to be displayed in browser...
+        public string Message { get; set; }
+    }
+}
